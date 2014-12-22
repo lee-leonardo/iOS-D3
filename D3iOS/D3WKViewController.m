a4daf32623b327427f85aad6bfbd2513ddb93034
Leonardo Lee <lee.leonardo.king@gmail.com>
2014-12-22 15:33:44 -0800

    The WebView is working. Problem with my implementation of the decidePolicyForNavigationAction, I was not switching on WKNavigationTypeOther, which is what is needed for navigating to links within the Bundle. I created copies of a few of the more dynamic projects for D3 to see how they behave as well.
    TODO: UIWebView implementation of the same deal.

--- D3iOS/D3WKViewController.m
+++ D3iOS/D3WKViewController.m
@@ -37,61 +37,56 @@
     _webView.UIDelegate = self;
     _webView.navigationDelegate = self;
     
-    NSURL *url = [NSURL URLWithString:@"https://www.google.com"];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [_webView loadRequest:request];
+//    NSURL *url = [NSURL URLWithString:@"https://www.google.com"];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    [_webView loadRequest:request];
+//    request = nil;
     
-//    NSString *indexPath = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
-//    NSURL *indexURL = [NSURL fileURLWithPath:indexPath];
-//    NSString *indexFile = [NSString stringWithContentsOfURL:indexURL encoding:NSUTF8StringEncoding error:nil];
+    NSString *indexPath = [[NSBundle mainBundle] pathForResource:@"simpleExample" ofType:@"html"];
+    NSURL *indexURL = [NSURL fileURLWithPath:indexPath];
+    NSString *indexFile = [NSString stringWithContentsOfURL:indexURL encoding:NSUTF8StringEncoding error:nil];
 //    NSLog(@"indexURL: %@", indexURL);
 //    NSLog(@"indexfile: %@", indexFile);
     
-//    [_webView loadHTMLString:indexFile baseURL:indexURL];
+    [_webView loadHTMLString:indexFile baseURL:indexURL];
     
 }
 
 #pragma mark - WKWebView
 //Where the App 'injects' itself.
 #pragma mark WKNavigationDelegate
--(void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation
-{
-    
-}
--(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
-{
-    
-}
-
--(void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error
-{
-    
-}
-
 -(void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
 {
-    //Handle navigation actions...
     switch (navigationAction.navigationType) {
-        case WKNavigationTypeLinkActivated:
-        case WKNavigationTypeReload:
-        case WKNavigationTypeFormSubmitted:
-            
-            //The problem I think is here...
+        case WKNavigationTypeOther:
+            //This is the action for loading from a local HTML document.
             decisionHandler(WKNavigationActionPolicyAllow);
             break;
             
+        case WKNavigationTypeBackForward:
         case WKNavigationTypeFormResubmitted:
+        case WKNavigationTypeFormSubmitted:
+        case WKNavigationTypeLinkActivated:
+        case WKNavigationTypeReload:
             decisionHandler(WKNavigationActionPolicyCancel);
             break;
-            
             
         default:
             break;
     }
+    
+    /*//Sample implementation for future...
+     NSURL *url = navigationAction.request.URL;
+     if (![url.host.lowercaseString hasPrefix:@"https://"]) {
+     decisionHandler(WKNavigationActionPolicyCancel);
+     return;
+     }
+     */
 }
 
 -(void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler
 {
+    //This is not being used at the moment, this would be what would be called right after the decidePolicy, this will return true as long as the type of file is visible at the moment.
     //Handle Reponse
     if (navigationResponse.canShowMIMEType) {
         decisionHandler(WKNavigationResponsePolicyAllow);
