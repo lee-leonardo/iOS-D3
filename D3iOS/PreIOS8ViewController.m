549d816fea48754f0170195c8921a68276769f8b
Leonardo Lee <lee.leonardo.king@gmail.com>
2015-01-05 13:24:07 -0800

    Tested on device, except for touches the WKWebView works like a charm, some slowdown on the more complex redrawing, but it works really well with SVGs. The UIWebView is a bit more problematic, it works well when I load from blit.it, it works really well. Unfortunately it does not work at all from device at the moment. I am thinking of a solution that wouldn't require online resources, but supporting older devices would be tricky. I am going to look at the framework that has a promise of cross-compatibility.
    Cordova sound promising as a solution... but I am running out of time so I'm going to focus in on making sure that an solution on the web can load up an be use for the time being?

--- D3iOS/PreIOS8ViewController.m
+++ D3iOS/PreIOS8ViewController.m
@@ -41,12 +41,19 @@
 //    Same idea as this, however this will be replaced directly by the html page. No linkage to the D3.
 //    NSString *d3Lib = [NSString stringWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"d3.min" withExtension:@".js"] encoding:NSUTF8StringEncoding error:NULL];
     
-    NSString *index = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
-    
-    NSURL *startPage = [NSURL fileURLWithPath:index];
-    NSString *page = [NSString stringWithContentsOfURL:startPage encoding:NSUTF8StringEncoding error:nil];
-    [_webView loadHTMLString:page baseURL:[[NSBundle mainBundle] resourceURL]];
+//    NSString *index = [[NSBundle mainBundle] pathForResource:@"index" ofType:@"html"];
+//    NSURL *startPage = [NSURL fileURLWithPath:index];
+//    NSString *page = [NSString stringWithContentsOfURL:startPage encoding:NSUTF8StringEncoding error:nil];
+//    [_webView loadHTMLString:page baseURL:[[NSBundle mainBundle] resourceURL]];
 //    [_webView loadHTMLString:page baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] resourcePath]]];
+
+/*
+    Loads the external resource without problem.
+ */
+    NSString *blit = @"http://test.blit.it/vis/chart4/";
+    NSURL *url = [NSURL URLWithString:blit];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [_webView loadRequest:request];
     
     /*
         An alternate way:
